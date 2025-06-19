78182e9c62c2f8b1fa29a28944f4330ac97d0969
Krutz <The1Krutz@gmail.com>
2025-06-19 14:31:49 -0400

    add double jump

--- RosellaGame/Characters/Player.cs
+++ RosellaGame/Characters/Player.cs
@@ -19,11 +19,15 @@ public partial class Player : CharacterBody2D {
   [Export]
   public float JumpVelocity = -300.0f;
 
+  [Export]
+  public float DoubleJumpVelocity = -200.0f;
+
   // Public Fields
 
   // Backing Fields
 
   // Private Fields
+  private int DoubleJumpsRemaining = 1;
 
   // Constructor
 
@@ -44,16 +48,24 @@ public partial class Player : CharacterBody2D {
     // Add the gravity.
     if (!IsOnFloor()) {
       velocity += GetGravity() * (float)delta;
+    } else {
+      // Restore double jump when player touches ground
+      DoubleJumpsRemaining = 1;
     }
 
     // Handle Jump.
-    if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()) {
-      velocity.Y = JumpVelocity;
+    if (Input.IsActionJustPressed("jump")) {
+      if (IsOnFloor()) {
+        velocity.Y = JumpVelocity;
+      } else if (DoubleJumpsRemaining > 0) {
+        DoubleJumpsRemaining--;
+        velocity.Y = DoubleJumpVelocity;
+      }
     }
 
     // Get the input direction and handle the movement/deceleration.
     // As good practice, you should replace UI actions with custom gameplay actions.
-    Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+    Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
     if (direction != Vector2.Zero) {
       velocity.X = direction.X * Speed;
     } else {
