e00b3e2bcbabec9c9e46d0a94a96b99a23f34d4a
Krutz <The1Krutz@gmail.com>
2025-06-23 23:05:57 -0400

    add comments

--- RosellaGame/Types/Damage.cs
+++ RosellaGame/Types/Damage.cs
@@ -3,11 +3,11 @@
 
 namespace RosellaGame.Types;
 
-/*
- * SO. Apparently a regular C# struct can't be passed as a parameter through a Godot Signal. There's some fancy engine
- * stuff Godot needs to do, and every Signal param needs to descend from Godot.GodotObject. Which I don't think a struct
- * can descend from anything? Anyway this might just need to be a class instead. Which, whatever it's fine.
- */
+/// <summary>
+/// SO. Apparently a regular C# struct can't be passed as a parameter through a Godot Signal. There's some fancy engine
+/// stuff Godot needs to do, and every Signal param needs to descend from Godot.GodotObject. Which I don't think a
+/// struct can descend from anything? Anyway this might just need to be a class instead. Which, whatever it's fine.
+/// </summary>
 public partial class Damage : Godot.GodotObject {
   public float Amount;
   public DamageType Type;
@@ -23,6 +23,10 @@ public partial class Damage : Godot.GodotObject {
   }
 }
 
+/// <summary>
+/// Right now it's every damage type in PF2e, but I don't expect to need all of them. Feel free to prune the list if you
+/// decide it's too much
+/// </summary>
 public enum DamageType {
   // Physical Damage
   Bludgeoning,
